9355c68267d4a512e7611193e695467e7d29b47e
Ollie Treend <ollie@treend.uk>
2025-01-26 19:55:34 +0000

    Deploy to GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,7 @@ import svelte from '@astrojs/svelte';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [svelte()]
+  integrations: [svelte()],
+  site: 'https://ollietreend.github.io',
+  base: 'count-script-lines',
 });
